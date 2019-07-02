07735ca1378536f811e937e5a1c02f8408da72cb
fachsimpeln <35078533+fachsimpeln@users.noreply.github.com>
2019-07-02 21:38:36 +0200

    [+] Added login system files and folders
    The main folders like css, includes and images were added and the <blank> files for the login system (neither coded nor finished).

---
+++ inc/header.inc.php
@@ -0,0 +1,13 @@
+<?php
+
+
+?>
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+     <head>
+          <meta charset="utf-8">
+          <title>mychat</title>
+
+          <!-- Include favicon -->
+          <link rel="icon" type="image/svg+xml" href="./img/mychat_logo.svg" sizes="any">
+     </head>

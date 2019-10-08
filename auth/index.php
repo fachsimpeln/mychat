03433a9c5c777fd88ae9e3c1a5ed49fa0914ad0b
fachsimpeln <35078533+fachsimpeln@users.noreply.github.com>
2019-10-08 17:00:27 +0200

    Added check if user-identfier is already in db logins

--- auth/index.php
+++ auth/index.php
@@ -1,22 +1,38 @@
 <?php
+
+     // INITIALIZE
+     require '../inc/db.inc.php';
+     $pdo = DatabaseConnection::connect();
+
      // GET USER AUTH
      $user = $_POST['user'];
      $password = $_POST['password'];
 
      // GENERATE RANDOM TOKEN (user-identifier enc-id, user-token 512bit random)
      $crypto = true;
-     $usertoken = openssl_random_pseudo_bytes(512, $crypto);
+     $usertoken = bin2hex(openssl_random_pseudo_bytes(512, $crypto));
 
 
      // CHECK IF ALREADY IN DATABASE AS A ID
      $bitlength = 32;
      $iD = true; // in Database
      while ($iD) {
-          $useridentifier = openssl_random_pseudo_bytes($bitlength, $crypto);
+          $iD = false;
+
+          $useridentifier = bin2hex(openssl_random_pseudo_bytes($bitlength, $crypto));
 
           // CHECK DATABASE AND SET $iD
+          $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+          $stmt = $pdo->prepare("SELECT * FROM `mc_logins` WHERE `login_userIdentifier`=:userID");
+          $stmt->bindParam(':userID', strval($useridentifier));
+          $stmt->execute();
 
-          $iD = false;
+          $rowCount = $stmt->rowCount();
+          if ($rowCount > 0) {
+               // User-Identifier already in database
+               $iD = true;
+               $bitlength += 1;
+          }
      }
 
      // SAVE IN DATABASE (table mc_logins)

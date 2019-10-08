167f7e2dcfbdbc3417eb3b1f3db2abad3215461b
fachsimpeln <35078533+fachsimpeln@users.noreply.github.com>
2019-10-08 16:11:26 +0200

    Authentication service for app added

---
+++ auth/index.php
@@ -0,0 +1,13 @@
+<?php
+     // GET USER AUTH
+     $user = $_POST['user'];
+     $password = $_POST['password'];
+
+     // GENERATE RANDOM TOKEN (user-identifier enc-id, user-token 512bit random)
+
+
+     // SAVE IN DATABASE (table mc_logins)
+
+     // OUTPUT TO USER
+
+?>

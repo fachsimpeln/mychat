5f21ee4f2da74fcc656a8fb5e8f5c1618b3110e4
fachsimpeln <35078533+fachsimpeln@users.noreply.github.com>
2019-10-08 16:15:40 +0200

    Added basic generation of tokens

--- auth/index.php
+++ auth/index.php
@@ -4,7 +4,20 @@
      $password = $_POST['password'];
 
      // GENERATE RANDOM TOKEN (user-identifier enc-id, user-token 512bit random)
+     $crypto = true;
+     $usertoken = openssl_random_pseudo_bytes(512, $crypto);
 
+
+     // CHECK IF ALREADY IN DATABASE AS A ID
+     $bitlength = 32;
+     $iD = true; // in Database
+     while ($iD) {
+          $useridentifier = openssl_random_pseudo_bytes($bitlength, $crypto);
+
+          // CHECK DATABASE AND SET $iD
+
+          $iD = false;
+     }
 
      // SAVE IN DATABASE (table mc_logins)
 

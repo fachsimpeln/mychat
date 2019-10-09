897315db7fa15d185da860da7e8debe3eaa1d539
fachsimpeln <35078533+fachsimpeln@users.noreply.github.com>
2019-10-09 20:20:51 +0200

    Login via Token in LoginHandler

--- inc/login.inc.php
+++ inc/login.inc.php
@@ -45,7 +45,51 @@
                return false;
           }
 
+          public function LoginToken($loginIdentifier, $loginToken)
+          {
 
+               $stmt = $this->pdo->prepare("SELECT `login_userIdentifier`, `login_token`, `login_expires`, `usr_id` FROM `mc_logins` WHERE `login_userIdentifier`=:loginUserID");
+               $stmt->bindParam(':loginUserID', strval($loginIdentifier));
+               $stmt->execute();
+
+               // CHECK IF LOGIN EXISTS
+               $rowCount = $stmt->rowCount();
+               if ($rowCount == 0) {
+                    $this->errorMessage = 'session_does_not_exist';
+                    return false;
+               }
+
+               // GET LOGIN INFORMATION FROM DATABASE
+               $loginInfo = $stmt->fetch(PDO::FETCH_ASSOC);
+
+               // CHECK EXPIRE
+               if (intval($loginInfo['login_expires']) < time()) {
+                    $this->errorMessage = 'session_expired';
+                    return false;
+               }
+
+               if (hash_equals($loginToken, $loginInfo['login_token'])) {
+                    $stmt = $this->pdo->prepare("SELECT `usr_email`, `usr_username`, `usr_password` FROM `mc_users` WHERE `usr_id`=:usrid");
+                    $stmt->bindParam(':usrid', strval($loginInfo['usr_id']));
+                    $stmt->execute();
+
+                    // CHECK IF USER EXISTS
+                    $rowCount = $stmt->rowCount();
+                    if ($rowCount == 0) {
+                         $this->errorMessage = 'user_does_not_exist';
+                         return false;
+                    }
+
+                    // GET USER INFORMATION FROM DATABASE
+                    $userInfo = $stmt->fetch(PDO::FETCH_ASSOC);
+
+                    $this->usrID = $userInfo['usr_id'];
+                    $this->usrUsername = $userInfo['usr_username'];
+                    return true;
+               }
+               $this->errorMessage = 'wrong_session_token';
+               return false;
+          }
 
      }
 ?>

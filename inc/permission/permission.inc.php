85bdbb7c130269c993ed28f5d9936ef1abaceb01
fachsimpeln <35078533+fachsimpeln@users.noreply.github.com>
2019-10-18 17:39:02 +0200

    Chat Permission for private single chat added

--- inc/permission/permission.inc.php
+++ inc/permission/permission.inc.php
@@ -8,16 +8,31 @@
 
           private $pdo = null;
 
-          private $permissions = array();
+          // ERROR MESSAGES
+          public $errorMessage = null;
 
           function __construct($pdo)
           {
                $this->pdo = $pdo;
           }
 
-          public function GetUserPermission($usrID)
+          public function GetChatPermission($usrID, $receiverUsername)
           {
-               return $this->permissions;
+               // CHECK IF THE TWO ARE FRIENDS
+               $stmt = $this->pdo->prepare("SELECT `fr_id` FROM `mc_friendlist` WHERE ((`usr_id1`=:senderid OR `usr_id2`=:senderid) AND (`usr_id1`= (SELECT `usr_id` FROM `mc_users` WHERE `usr_username`=:receiverUsername) OR `usr_id2`=(SELECT `usr_id` FROM `mc_users` WHERE `usr_username`=:receiverUsername)))");
+               
+               $stmt->bindParam(':senderid', strval($usrID));
+               $stmt->bindParam(':receiverUsername', strval($receiverUsername));
+               $stmt->execute();
+
+               // CHECK IF USER EXISTS
+               $rowCount = $stmt->rowCount();
+               if ($rowCount == 0) {
+                    $this->errorMessage = 'no_permission';
+                    return false;
+               }
+
+               return true;
           }
 
           public function SetUserPermission($usrID, $permission)

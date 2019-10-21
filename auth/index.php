16ba26e1edf9bdfe7826028f30ac830dafa0a261
fachsimpeln <35078533+fachsimpeln@users.noreply.github.com>
2019-10-21 19:56:26 +0200

    Error Output for testing purposes

--- auth/index.php
+++ auth/index.php
@@ -14,7 +14,7 @@
      $loginHandler = new LoginHandler($pdo);
      if (!$loginHandler->LoginUser($email, $password)) {
           // NOT AUTHENTICATED
-          die('no_auth');
+          die('no_auth' . '<br />' . $loginHandler->errorMessage);
      }
      // GET USR-ID
      $usrID = $loginHandler->usrID;

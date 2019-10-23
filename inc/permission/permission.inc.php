c3b7f20beada499cb1766f88b5f08b6570b89b3b
fachsimpeln <35078533+fachsimpeln@users.noreply.github.com>
2019-10-23 17:54:07 +0200

    PHPdoc tags added

--- inc/permission/permission.inc.php
+++ inc/permission/permission.inc.php
@@ -2,20 +2,38 @@
 
      /**
       * Permission Handler
+      * This class handles the permissions for users, groups and admin
+      *
+      * @author fachsimpeln
+      * @category Permission System
+      * @version permission.system.db, v1.0, 2019-10-23
       */
      class PermissionHandler
      {
 
-          private $pdo = null;
+          private $pdo = null; // PDO object for db connection via constructor
 
-          // ERROR MESSAGES
-          public $errorMessage = null;
+          public $errorMessage = null; // Error-Message after error
 
+          /**
+          * Constructor
+          * Initialize PermissionHandler object
+          *
+          * @param PDO $pdo PDO with active database connection
+          */
           function __construct($pdo)
           {
                $this->pdo = $pdo;
           }
 
+          /**
+          * LoginUser
+          * Checks if two users are allowed to communicate (are friends)
+          *
+          * @param string $usrID Internal db user id of sender
+          * @param string $receiverUsername Username of receiver
+          * @return bool Has permission to write (true), no permission (false)
+          */
           public function GetChatPermission($usrID, $receiverUsername)
           {
                // CHECK IF THE TWO ARE FRIENDS

d39971eb219fb673ea15554f91aac56b7818482f
fachsimpeln <35078533+fachsimpeln@users.noreply.github.com>
2019-10-23 17:50:38 +0200

    Check for accepted friendship

--- inc/permission/permission.inc.php
+++ inc/permission/permission.inc.php
@@ -19,8 +19,8 @@
           public function GetChatPermission($usrID, $receiverUsername)
           {
                // CHECK IF THE TWO ARE FRIENDS
-               $stmt = $this->pdo->prepare("SELECT `fr_id` FROM `mc_friendlist` WHERE ((`usr_id1`=:senderid OR `usr_id2`=:senderid) AND (`usr_id1`= (SELECT `usr_id` FROM `mc_users` WHERE `usr_username`=:receiverUsername) OR `usr_id2`=(SELECT `usr_id` FROM `mc_users` WHERE `usr_username`=:receiverUsername)))");
-               
+               $stmt = $this->pdo->prepare("SELECT `fr_id` FROM `mc_friendlist` WHERE ((`usr_id1`=:senderid OR `usr_id2`=:senderid) AND (`usr_id1`= (SELECT `usr_id` FROM `mc_users` WHERE `usr_username`=:receiverUsername) OR `usr_id2`=(SELECT `usr_id` FROM `mc_users` WHERE `usr_username`=:receiverUsername))) AND `fr_accepted`=1");
+
                $stmt->bindParam(':senderid', strval($usrID));
                $stmt->bindParam(':receiverUsername', strval($receiverUsername));
                $stmt->execute();

178ee8190f5a8fbb55f5e437034fcd004b4f6a30
fachsimpeln <35078533+fachsimpeln@users.noreply.github.com>
2019-10-23 17:57:32 +0200

    PHPdoc tags added

--- inc/security/xss.inc.php
+++ inc/security/xss.inc.php
@@ -1,7 +1,14 @@
 <?php
+     /**
+     * PreventXSS
+     * Prevent XSS attacks by replacing all html tag with the special chars
+     * ! Always use when output to user !
+     *
+     * @param string $text Text to be filtered
+     * @return string Filtered text
+     */
      function PreventXSS($text)
      {
           return htmlspecialchars($text, ENT_QUOTES);
      }
-
 ?>

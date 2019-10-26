0683707d398cf08e59832963ed02c8c931a283c3
fachsimpeln <35078533+fachsimpeln@users.noreply.github.com>
2019-10-26 18:27:02 +0200

    Test Form for message sending

--- index.php
+++ index.php
@@ -18,4 +18,14 @@
           <h1>mychat</h1>
           <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
      </body>
+
+     <h2>Send message - Testing</h2>
+     <form class="" action="./messages/send/" method="post">
+          <input type="text" name="receiver" value="" placeholder="Receiver Username...">
+          <br /><br />
+          <textarea name="message" rows="8" cols="80" placeholder="Message text..."></textarea>
+          <br /><br />
+          <input type="text" name="rtype" value="private"><br /><br />
+          <input type="submit" name="" value="Send Message!">
+     </form>
 </html>

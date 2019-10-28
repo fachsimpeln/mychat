8cd7de20fc5470d9bacc11c3fb0ffb54ef4e99f1
fachsimpeln <35078533+fachsimpeln@users.noreply.github.com>
2019-10-28 19:45:00 +0100

    Read messages in conversations

---
+++ messages/chat/index.php
@@ -0,0 +1,46 @@
+<?php
+
+     // INITIALIZE
+     require '../../inc/db.inc.php';
+     require '../../inc/login.inc.php';
+     require '../../inc/messages/messages.inc.php';
+     require '../../inc/security/xss.inc.php';
+     $pdo = DatabaseConnection::connect();
+
+     // CHECK USER AUTH
+     $loginHandler = new LoginHandler($pdo);
+
+     $loginInfo = $loginHandler->GetToken($_POST);
+     if (!$loginHandler->LoginToken($loginInfo['lid'], $loginInfo['lto'])) {
+          // NOT AUTHENTICATED
+          die('no_auth');
+     }
+     // GET USR-ID
+     $usrID = $loginHandler->usrID;
+
+     // CHECK PERMISSION
+     $permissions = new PermissionHandler($pdo);
+     if ($receiverType === 'private') {
+          // CHECK IF RIGHT TO SEND
+          if (!$permissions->GetChatPermission($usrID, $receiverUsername)) {
+               // NO PERMISSION
+               die('no_permission');
+          }
+          $text = $_REQUEST['message'];
+
+          $messages = new MessageHandler($pdo);
+          $receiverID = $messages->GetUserID($receiverUsername);
+
+          $conversationPath = '../messages/conversation/' . $messages->GetConversationPath($usrID, $receiverID);
+
+          $messages->ReadConversation($usrID, $receiverID);
+
+          /* Output all unread messages and then up to x more (e.g. 15)
+           * Then lazyload new message via an offset parameter (handled by javascript)
+           */
+
+
+     }
+
+
+?>

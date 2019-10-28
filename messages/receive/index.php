b6a1bc622e8b4dea69e1f550f22cd827f4b9321e
fachsimpeln <35078533+fachsimpeln@users.noreply.github.com>
2019-10-28 18:56:32 +0100

    Receive unread messages from all chats

--- messages/receive/index.php
+++ messages/receive/index.php
@@ -1,5 +1,56 @@
 <?php
 
-     
+     /**
+     * Receive
+     * Check for new messages in all chats
+     */
+
+     // INITIALIZE
+     require '../../inc/db.inc.php';
+     require '../../inc/login.inc.php';
+     require '../../inc/messages/messages.inc.php';
+     require '../../inc/security/xss.inc.php';
+     $pdo = DatabaseConnection::connect();
+
+     // CHECK USER AUTH
+     $loginHandler = new LoginHandler($pdo);
+
+     $loginInfo = $loginHandler->GetToken($_POST);
+     if (!$loginHandler->LoginToken($loginInfo['lid'], $loginInfo['lto'])) {
+          // NOT AUTHENTICATED
+          die('no_auth');
+     }
+     // GET USR-ID
+     $usrID = $loginHandler->usrID;
+
+     $unreadPath = '../messages/user/' . $usrID . '/unread.json';
+     $unread = ReadUnreadToFile($unreadPath, $usrID);
+     $output = array();
+
+     if ($unread == null || count($unread) == 0) {
+          die(json_encode($output));
+     }
+
+     $messages = new MessageHandler($pdo);
+     foreach ($unread as $key => $value) {
+          $output[$messages->GetUsername($key)] = $value;
+     }
+     die(json_encode($output));
+
+     /**
+     * ReadUnreadToFile
+     * Reads entry for unread messages
+     *
+     * @param string $path Path to unread file
+     * @param int $usrID Logged in user-id
+     * @return array Returns all unread messages (on error null)
+     */
+     function ReadUnreadToFile($path, $usrID) {
+          $json = null;
+          if (file_exists($path)) {
+               $json = json_decode(file_get_contents($path), true);
+          }
+          return $json;
+     }
 
 ?>

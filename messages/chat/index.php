abab93025d0ff9e4d62647c520b4da2ef47e01cb
fachsimpeln <35078533+fachsimpeln@users.noreply.github.com>
2019-11-08 16:53:04 +0100

    Autoloader updated

--- messages/chat/index.php
+++ messages/chat/index.php
@@ -1,10 +1,7 @@
 <?php
 
      // INITIALIZE
-     require '../../inc/db.inc.php';
-     require '../../inc/login.inc.php';
-     require '../../inc/messages/messages.inc.php';
-     require '../../inc/security/xss.inc.php';
+     require '../../inc/autoload.php';
      $pdo = DatabaseConnection::connect();
 
      // CHECK USER AUTH
